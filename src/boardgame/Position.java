849afacf491b4d1a20e89d58eb91d8c1d32d2262
Vesper03 <xxndtry33@gmail.com>
2022-03-04 14:48:41 -0300

    First class: Position

---
+++ src/boardgame/Position.java
@@ -0,0 +1,45 @@
+package boardgame;
+
+public class Position {
+	
+//class//
+	private int row;
+	private int column;
+	
+	
+//constructor//
+
+	public Position(int row, int column) {
+	
+		this.row = row;
+		this.column = column;
+	}
+
+//gets and sets//
+	
+	public int getRow() {
+		return row;
+	}
+
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+
+	public int getColumn() {
+		return column;
+	}
+
+
+	public void setColumn(int column) {
+		this.column = column;
+	}
+
+
+	@Override
+	public String toString() {
+		return row + ", " + column;
+	}
+	
+}
