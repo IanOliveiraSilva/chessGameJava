a49618cfee7e7ce29d4476128fc8db9241056257
Vesper03 <xxndtry33@gmail.com>
2022-03-07 15:16:57 -0300

    Handling exception and clearing screen

--- src/application/UI.java
+++ src/application/UI.java
@@ -32,6 +32,11 @@ public class UI {
 
 //methods//
 	
+	public static void clearScreen() {
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
+	}
+	
 	public static ChessPosition readCHessPosition(Scanner sc) {
 		try {
 			String s = sc.nextLine();
